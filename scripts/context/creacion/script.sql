3fc8171be24f3d5111d25e60570f3d5ab44298ae
Ubuntu <ubuntu@ip-172-31-21-150.us-east-2.compute.internal>
2020-09-09 07:36:07 +0000

    ficheros

--- scripts/context/creacion/script.sql
+++ scripts/context/creacion/script.sql
@@ -368,8 +368,294 @@ from
 where 
     contains(nombre,'ivan de ayuste',27)>0;
     
-    
+/*    
 BUSQUEDA:
     ivan
         -> tanto en nombre como en apellidos
     -> Quiero premiar los resultados en los que ivan esté contenido en el nombre
+    
+    
+| Usuarios                      |
+| Nombre      | Apellidos       |
+| Ivan.       | Osuna.          |. 1
+| Ivan.       | Garcia.         |. 2
+| Lucas.      | de Ivan.        |. 3
+
+QUERY: ivan (Nombre*2 + Apellidos)
+
+ID.  SCORE
+1.   10
+2.   10
+3.   5
+
+
+INDICES: 
+--------
+ 2 Indices: NOMBRE_IDX
+            APELLIDOS_IDX
+ Solucion IDEAL:
+    1 INDICE multicolumna -> NOMBRE_COMPLETO_IDX
+        SECTIONER -> Separaba bloques de contenido dentro de un indice (habitualmente se usa para marcas HTML, XML)
+
+QUERY
+------
+SELECT ID, SCORE(1) * 2 + SCORE(2)
+FROM USUARIOS
+WHERE 
+       CONTAINS (nombre   , 'ivan', 1) > 0   ---> Nombre_idx
+    OR CONTAINS (apellidos, 'ivan', 2) > 0   ---> Apellidos_idx
+
+
+      CHAPUZA !!!!!!!
+      El performance es malo!
+*/
+SELECT ID, SCORE(1)
+FROM USUARIOS
+WHERE 
+       CONTAINS (nombre_completo   , 'ivan', 1) > 0   ---> Nombre_idx
+
+/*
+    GUAY en cuanto a RENDIMIENTO !!!!
+    Problema: Como controlo que una columna tenga más score que otra?¿?¿?¿? 
+    Asi no lo estoy controlando
+*/
+
+
+drop UsuariosSeparados;
+create table UsuariosSeparados (
+    Id number primary key, 
+    Pais varchar2(2), 
+    Nombre varchar2(200),
+    Apellidos varchar2(200)
+);
+insert into UsuariosSeparados values (1, 'ES', 'Ivan', 'Osuna');
+insert into UsuariosSeparados values (2, 'ES', 'Luis', 'Osuna-Osuna');
+insert into UsuariosSeparados values (3, 'ES', 'Ivan', 'Ayuste');
+insert into UsuariosSeparados values (4, 'UK', 'Ivan', 'de Ayuste');
+insert into UsuariosSeparados values (5, 'UK', 'Iv'||unistr('\00C1')||'n', 'de Ayuste');
+insert into UsuariosSeparados values (6, 'ES', 'Osuna', 'Ayuste');
+insert into UsuariosSeparados values (7, 'ES', 'Ayuste', 'Ivan');
+insert into UsuariosSeparados values (8, 'ES', 'Ivan', 'Casas');
+insert into UsuariosSeparados values (9, 'ES', 'Ivan', 'Casa');
+insert into UsuariosSeparados values (10, 'ES', 'Ivan', 'Casitas');
+
+
+
+exec ctx_ddl.create_preference( 'mi_lexer_de_nombres' , 'BASIC_LEXER' );
+exec ctx_ddl.set_attribute(     'mi_lexer_de_nombres' , 'BASE_LETTER' , 'TRUE' );
+exec ctx_ddl.set_attribute(     'mi_lexer_de_nombres' , 'INDEX_STEMS' , 'SPANISH' );
+
+exec ctx_ddl.create_preference( 'mi_wordlist_de_nombres' , 'BASIC_WORDLIST' );
+exec ctx_ddl.set_attribute(     'mi_wordlist_de_nombres' , 'STEMMER' , 'SPANISH' );
+exec ctx_ddl.set_attribute(     'mi_wordlist_de_nombres' , 'FUZZY_MATCH' , 'SPANISH' );
+exec ctx_ddl.set_attribute(     'mi_wordlist_de_nombres' , 'FUZZY_SCORE' , '1' );
+exec ctx_ddl.set_attribute(     'mi_wordlist_de_nombres' , 'FUZZY_NUMRESULTS' , '5000' );
+exec ctx_ddl.set_attribute(     'mi_wordlist_de_nombres' , 'PREFIX_INDEX' , 'TRUE' );
+exec ctx_ddl.set_attribute(     'mi_wordlist_de_nombres' , 'PREFIX_MIN_LENGTH' , '3' );
+exec ctx_ddl.set_attribute(     'mi_wordlist_de_nombres' , 'PREFIX_MAX_LENGTH' , '20' );
+exec ctx_ddl.set_attribute(     'mi_wordlist_de_nombres' , 'SUBSTRING_INDEX' , 'TRUE' );
+
+exec ctx_ddl.create_stoplist('palabras_vacias_en_nombres');
+exec ctx_ddl.add_stopword('palabras_vacias_en_nombres', 'de');
+exec ctx_ddl.add_stopword('palabras_vacias_en_nombres', 'la');
+exec ctx_ddl.add_stopword('palabras_vacias_en_nombres', 'las');
+exec ctx_ddl.add_stopword('palabras_vacias_en_nombres', 'los');
+exec ctx_ddl.add_stopword('palabras_vacias_en_nombres', 'el');
+exec ctx_ddl.add_stopword('palabras_vacias_en_nombres', 'y');
+
+/*
+    DATASTORES:
+        ctxsys.default_datastore:        Trabaja con 1 columna
+        ctxsys.multi_column_datastore:   Trabaja con varias columnas
+*/
+
+exec ctx_ddl.create_preference( 'datastore_nombre_completo' , 'MULTI_COLUMN_DATASTORE' );
+exec ctx_ddl.set_attribute(     'datastore_nombre_completo' , 'COLUMNS' , 'nombre,apellidos' );
+
+/*Columna dummy para su uso en busquedas*/
+ALTER TABLE usuariosseparados ADD nombre_completo VARCHAR2(1);
+
+drop index nombrecompleto_sep_idx;
+create index nombrecompleto_sep_idx on usuariosseparados(nombre_completo)
+    indextype is ctxsys.context parameters('
+        datastore datastore_nombre_completo
+        sync (on commit)
+        lexer mi_lexer_de_nombres
+        wordlist mi_wordlist_de_nombres
+        stoplist palabras_vacias_en_nombres
+    ');
+
+
+drop index nombre_sep_idx;
+create index nombre_sep_idx on usuariosseparados(nombre)
+    indextype is ctxsys.context parameters('
+        sync (on commit)
+        lexer mi_lexer_de_nombres
+        wordlist mi_wordlist_de_nombres
+        stoplist palabras_vacias_en_nombres
+    ');
+
+drop index apellidos_sep_idx;
+create index apellidos_sep_idx on usuariosseparados(apellidos)
+    indextype is ctxsys.context parameters('
+        sync (on commit)
+        lexer mi_lexer_de_nombres
+        wordlist mi_wordlist_de_nombres
+        stoplist palabras_vacias_en_nombres
+    ');
+
+
+
+SELECT ID, SCORE(1)
+FROM USUARIOSSEPARADOS
+WHERE 
+       CONTAINS (nombre_completo   , 'ivan', 1) > 0;   ---> Nombre_idx
+       
+       
+
+SELECT ID, SCORE(1)*10 + SCORE(2) as relevancia 
+FROM USUARIOSSEPARADOS
+WHERE 
+          CONTAINS (nombre      , 'ivan', 1) > 0        
+       OR CONTAINS (apellidos   , 'ivan', 2) > 0
+ORDER BY relevancia DESC;
+/* 
+    UNA CHAPU -> Rendimiento es muy mejorable
+
+Juntar las 2 tablas OR  -> Union           -> Poner unos cuantos delante y otros detras
+                           select DISTINCT -> Eliminar los duplicados
+                                  sort ID  -> Peor operación que puedo pedir a una base de datos
+
+Combinar 2 tablas   AND -> JOIN
+
+En ordenar 10 datos tarda 10 seg
+En ordenar 20 datos tarda 30 seg
+
+*/
+/*
+PARA RESOLVER ESTO VAMOS A TRABAJAR CON SECTIONERS
+*/
+drop textos_marcados;
+CREATE TABLE textos_marcados(
+    Id number primary key, 
+    Texto varchar2(4000)
+);
+insert into textos_marcados values (1, '<documento><titulo>Frutas</titulo><contenido>Algunas frutas son: Fresas, melón, manzanas platanos, peras.</contenido></documento>');
+insert into textos_marcados values (2, '<documento><titulo>Receta de tarta de manzana</titulo><contenido>Ingredientes: manzanas, harina, huevos, leche...</contenido></documento>');
+
+drop   index contenido_textos_marcados_idx;
+create index contenido_textos_marcados_idx on textos_marcados(Texto)
+    indextype is ctxsys.context parameters('
+        sync (on commit)
+        lexer mi_lexer_de_nombres
+        wordlist mi_wordlist_de_nombres
+        stoplist palabras_vacias_en_nombres
+    ');
+
+
+SELECT ID, SCORE(1)
+FROM textos_marcados
+WHERE 
+       CONTAINS (texto   , 'manzana', 1) > 0; 
+       
+SELECT ID, SCORE(1)
+FROM textos_marcados
+WHERE 
+       CONTAINS (texto   , 'titulo', 1) > 0; 
+/*
+<documento>
+    <titulo>Frutas</titulo>
+    <contenido>
+        Algunas frutas son: 
+            Fresas, 
+            melón, 
+            manzanas 
+            platanos, 
+            peras
+    </contenido>
+</documento>
+
+<documento>
+    <titulo>Receta de tarta de manzana</titulo>
+    <contenido>
+        Ingredientes: 
+            manzanas, 
+            harina, 
+            huevos, 
+            leche
+    </contenido>
+</documento>
+*/
+exec ctx_ddl.create_section_group('seccionador_documentos' , 'AUTO_SECTION_GROUP');
+
+drop   index contenido_textos_marcados_idx;
+create index contenido_textos_marcados_idx on textos_marcados(Texto)
+    indextype is ctxsys.context parameters('
+        section group seccionador_documentos
+        sync (on commit)
+        lexer mi_lexer_de_nombres
+        wordlist mi_wordlist_de_nombres
+        stoplist palabras_vacias_en_nombres
+    ');
+    
+
+SELECT ID, SCORE(1)
+FROM textos_marcados
+WHERE 
+       CONTAINS (texto   , '$manzana', 1) > 0; 
+SELECT ID, SCORE(1)
+
+FROM textos_marcados
+WHERE 
+       CONTAINS (texto   , '$manzana WITHIN titulo', 1) > 0; 
+
+
+SELECT ID, SCORE(1)
+FROM textos_marcados
+WHERE 
+       CONTAINS (texto   , 'harina WITHIN contenido', 1) > 0; 
+       
+
+SELECT ID, SCORE(1)
+FROM textos_marcados
+WHERE 
+       CONTAINS (texto   , '($manzana within titulo) * 2', 1) > 0; 
+
+
+SELECT ID, SCORE(1)
+FROM textos_marcados
+WHERE 
+       CONTAINS (texto   , '($manzana within titulo) * 10 or ($manzana within contenido)', 1) > 0; 
+       
+/*
+    SUPER GUAY !!!!!!  MUY RAPIDITO 
+*/
+
+
+/*
+    Vamos a aplicar esto al ejeplo de la multicolumna del nombre y apellidos
+*/
+exec ctx_ddl.create_section_group('seccionador_nombres_completos' , 'AUTO_SECTION_GROUP');
+
+drop index nombrecompleto_sep_idx;
+create index nombrecompleto_sep_idx on usuariosseparados(nombre_completo)
+    indextype is ctxsys.context parameters('
+        datastore datastore_nombre_completo
+        sync (on commit)
+        lexer mi_lexer_de_nombres
+        wordlist mi_wordlist_de_nombres
+        stoplist palabras_vacias_en_nombres
+        section group seccionador_nombres_completos
+    ');
+
+SELECT ID, SCORE(1)
+FROM USUARIOSSEPARADOS
+WHERE 
+       CONTAINS (nombre_completo   , 'ivan within apellidos', 1) > 0;  
+
+
+SELECT ID, SCORE(1)
+FROM USUARIOSSEPARADOS
+WHERE 
+       CONTAINS (nombre_completo   , '(ivan within apellidos) * 10 or (ivan within nombre)', 1) > 0;  
+              
