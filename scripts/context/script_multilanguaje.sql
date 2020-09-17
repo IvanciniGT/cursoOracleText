20391bd50333b447a68acdb72990f6faf5f5b7b5
Ubuntu <ubuntu@ip-172-31-21-150.us-east-2.compute.internal>
2020-09-17 17:14:06 +0000

    idiomas

--- scripts/context/script_multilanguaje.sql
+++ scripts/context/script_multilanguaje.sql
@@ -60,7 +60,75 @@ BEGIN
     cargar_fichero_por_idiomas ( 'spanish' , 'CARPETA_FICHEROS_HTML', 'carrilleras.html'  , new_id);
     cargar_fichero_por_idiomas ( 'spanish' , 'CARPETA_FICHEROS_HTML', 'pisto.html'  , new_id);
 
-    DBMS_OUTPUT.PUT_LINE('Nuevo id' || new_id);
+    DBMS_OUTPUT.PUT_LINE('Nuevo id: ' || new_id);
 END; 
 /
 COMMIT;
+
+
+/*
+INDICES
+*/
+
+/*LEXER INGLES*/
+exec ctx_ddl.create_preference('en_mi_blob_lexer', 'BASIC_LEXER');
+exec ctx_ddl.set_attribute(    'en_mi_blob_lexer', 'BASE_LETTER', 'TRUE');
+exec ctx_ddl.set_attribute(    'en_mi_blob_lexer', 'INDEX_STEMS', 'ENGLISH');        
+exec ctx_ddl.set_attribute(    'en_mi_blob_lexer', 'MIXED_CASE', 'TRUE');        
+/*LEXER  ESPAÑOL*/
+exec ctx_ddl.create_preference('es_mi_blob_lexer', 'BASIC_LEXER');
+exec ctx_ddl.set_attribute(    'es_mi_blob_lexer', 'BASE_LETTER', 'TRUE');
+exec ctx_ddl.set_attribute(    'es_mi_blob_lexer', 'INDEX_STEMS', 'SPANISH');        
+exec ctx_ddl.set_attribute(    'es_mi_blob_lexer', 'MIXED_CASE', 'TRUE');        
+/* MULTILEXER*/
+exec ctx_ddl.create_preference('mi_blob_lexer_multi_idioma', 'MULTI_LEXER');
+exec ctx_ddl.add_sub_lexer(    'mi_blob_lexer_multi_idioma' , 'english', 'en_mi_blob_lexer'); 
+exec ctx_ddl.add_sub_lexer(    'mi_blob_lexer_multi_idioma' , 'spanish', 'es_mi_blob_lexer'); 
+
+/*WORDLIST*/
+exec ctx_ddl.create_stoplist('en_blob_palabras_vacias');
+exec ctx_ddl.add_stopword(   'en_blob_palabras_vacias', 'the');
+exec ctx_ddl.add_stopword(   'en_blob_palabras_vacias', 'a');
+exec ctx_ddl.add_stopword(   'en_blob_palabras_vacias', 'an');
+exec ctx_ddl.add_stopword(   'en_blob_palabras_vacias', 'these');
+
+exec ctx_ddl.create_stoplist('es_blob_palabras_vacias');
+exec ctx_ddl.add_stopword(   'es_blob_palabras_vacias', 'the');
+exec ctx_ddl.add_stopword(   'es_blob_palabras_vacias', 'a');
+exec ctx_ddl.add_stopword(   'es_blob_palabras_vacias', 'an');
+exec ctx_ddl.add_stopword(   'es_blob_palabras_vacias', 'these');
+
+/*Preferencias genericas*/
+exec ctx_ddl.create_preference('mi_blob_datasource_ficheros' , 'DIRECT_DATASTORE');
+
+exec ctx_ddl.create_preference('mi_blob_wordlist' , 'BASIC_WORDLIST');
+exec ctx_ddl.set_attribute(    'mi_blob_wordlist' , 'STEMMER', 'ENGLISH');        
+exec ctx_ddl.set_attribute(    'mi_blob_wordlist' , 'FUZZY_MATCH', 'ENGLISH');    
+exec ctx_ddl.set_attribute(    'mi_blob_wordlist' , 'FUZZY_SCORE', '1');
+exec ctx_ddl.set_attribute(    'mi_blob_wordlist' , 'FUZZY_NUMRESULTS', '5000');
+exec ctx_ddl.set_attribute(    'mi_blob_wordlist' , 'PREFIX_INDEX' , 'TRUE' );    
+exec ctx_ddl.set_attribute(    'mi_blob_wordlist' , 'PREFIX_MIN_LENGTH' , '3' );  
+exec ctx_ddl.set_attribute(    'mi_blob_wordlist' , 'PREFIX_MAX_LENGTH' , '8' );  
+exec ctx_ddl.set_attribute(    'mi_blob_wordlist' , 'SUBSTRING_INDEX' , 'TRUE' );
+
+exec ctx_ddl.create_section_group('mi_seccionador_blob' , 'NULL_SECTION_GROUP'); 
+exec ctx_ddl.add_special_section( 'mi_seccionador_blob' , 'SENTENCE'); 
+exec ctx_ddl.add_special_section( 'mi_seccionador_blob' , 'PARAGRAPH'); 
+
+
+
+
+DROP INDEX fichero_blob_idx;
+CREATE INDEX fichero_blob_idx ON multilenguaje (contenido)
+INDEXTYPE IS CTXSYS.CONTEXT PARAMETERS
+(
+'
+    filter ctxsys.AUTO_FILTER
+    datastore mi_blob_datasource_ficheros
+    section group mi_seccionador_blob
+    sync(on commit) 
+    lexer mi_blob_lexer
+    wordlist mi_blob_wordlist
+    stoplist blob_palabras_vacias
+'
+); 
